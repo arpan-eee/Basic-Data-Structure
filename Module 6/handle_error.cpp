bdca289b3bd78483d8ee9df5384fe1b558931a58
Arpan Chakraborty <arpan.chakraborty9876@gmail.com>
2023-06-13 18:11:53 +0600

    Linked List Day 2

---
+++ Module 6/handle_error.cpp
@@ -0,0 +1,147 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+void insert_at_tail(Node *&head, int v)
+{
+    Node *newNode = new Node(v);
+    if (head == NULL)
+    {
+        head = newNode;
+        return;
+    }
+
+    Node *tmp = head;
+    while (tmp->next != NULL)
+    {
+        tmp = tmp->next;
+    }
+    // tmp ekhon last node e
+    tmp->next = newNode;
+}
+void delete_a_position(Node *&head, int p)
+{
+    if (p == 0)
+    {
+        Node *deleteNode = head;
+        if (head != NULL)
+        {
+            head = head->next;
+            delete deleteNode;
+            return;
+        }
+        else
+        {
+            cout << "Nothing To Delete" << endl;
+            return;
+        }
+    }
+    int i = 1;
+    Node *tmp = head;
+    while (i < p)
+    {
+        tmp = tmp->next;
+        if (tmp == NULL)
+        {
+            cout << "Invalid Info" << endl;
+            return;
+        }
+        i++;
+    }
+    Node *deleteNode = tmp->next;
+    tmp->next = tmp->next->next;
+    delete deleteNode;
+    cout << "Deleted at required Position" << endl;
+}
+void insert_at_pos(Node *&head, int v, int p)
+{
+    Node *newNode = new Node(v);
+    if (p == 0)
+    {
+        newNode->next = head;
+        head = newNode;
+        return;
+    }
+    int i = 1;
+    Node *tmp = head;
+    while (i < p)
+    {
+        tmp = tmp->next;
+        if (tmp == NULL)
+        {
+            cout << "Invalid Info" << endl;
+            return;
+        }
+        i++;
+    }
+    // tmp ekhon pos node e
+    newNode->next = tmp->next;
+    tmp->next = newNode;
+    cout << "Inserted at required Position" << endl;
+}
+void print_linked_list(Node *head)
+{
+    cout << "Your Linked List: ";
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
+int main()
+{
+    Node *head = NULL;
+    while (true)
+    {
+        cout << "Option 1: Insert at Tail" << endl;
+        cout << "Option 2: Print Linked List" << endl;
+        cout << "Option 3: Insert at Position" << endl;
+        cout << "Option 4: Delete at Position" << endl;
+        cout << "Option 5: Terminate" << endl;
+        int op;
+        cin >> op;
+        if (op == 1)
+        {
+            cout << "Please enter value: ";
+            int v;
+            cin >> v;
+            insert_at_tail(head, v);
+            cout << "Inserted at tail" << endl;
+        }
+        else if (op == 2)
+        {
+            print_linked_list(head);
+            cout << "Link List Printed" << endl;
+        }
+        else if (op == 3)
+        {
+            cout << "Please enter position and then value: ";
+            int v, p;
+            cin >> p >> v;
+            insert_at_pos(head, v, p);
+        }
+        else if (op == 4)
+        {
+            cout << "Please enter position that you want to delete: ";
+            int p;
+            cin >> p;
+            delete_a_position(head, p);
+        }
+        else if (op == 5)
+        {
+            break;
+        }
+    }
+    return 0;
+}
---
+++ Module 6/insert_at_any_position.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+void insert_at_tail(Node *&head, int v)
+{
+    Node *newNode = new Node(v);
+    if (head == NULL)
+    {
+        head = newNode;
+        return;
+    }
+
+    Node *tmp = head;
+    while (tmp->next != NULL)
+    {
+        tmp = tmp->next;
+    }
+    // tmp ekhon last node e
+    tmp->next = newNode;
+}
+void insert_at_pos(Node *&head, int v, int p)
+{
+    Node *newNode = new Node(v);
+    if (p == 0)
+    {
+        newNode->next=head;
+        head = newNode;
+        return;
+    }
+    int i = 1;
+    Node *tmp = head;
+    while (i < p)
+    {
+        tmp = tmp->next;
+        i++;
+    }
+    // tmp ekhon pos node e
+    newNode->next = tmp->next;
+    tmp->next = newNode;
+}
+void print_linked_list(Node *head)
+{
+    cout << "Your Linked List: ";
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
+int main()
+{
+    Node *head = NULL;
+    while (true)
+    {
+        cout << "Option 1: Insert at Tail" << endl;
+        cout << "Option 2: Print Linked List" << endl;
+        cout << "Option 3: Insert at Position" << endl;
+        cout << "Option 4: Terminate" << endl;
+        int op;
+        cin >> op;
+        if (op == 1)
+        {
+            cout << "Please enter value: ";
+            int v;
+            cin >> v;
+            insert_at_tail(head, v);
+            cout << "Inserted at tail" << endl;
+        }
+        else if (op == 2)
+        {
+            print_linked_list(head);
+            cout << "Link List Printed" << endl;
+        }
+        else if (op == 3)
+        {
+            cout << "Please enter position and then value: ";
+            int v, p;
+            cin >> p >> v;
+            insert_at_pos(head, v, p);
+            cout << "Inserted at required Position" << endl;
+        }
+        else if (op == 4)
+        {
+            break;
+        }
+    }
+    return 0;
+}
